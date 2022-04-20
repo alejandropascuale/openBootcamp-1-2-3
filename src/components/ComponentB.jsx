8373ae385dc4578d8c3c4d9eabd73ec4878b9614
alejandropascuale <apascuale@outlook.com>
2022-04-20 15:44:57 -0300

    change name of state in componentB

--- src/components/ComponentB.jsx
+++ src/components/ComponentB.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Contact } from '../models/contact.class'
 
 function ComponentB({ estado }) {
-    const [state, setState] = useState(estado);
+    const [status, setStatus] = useState(estado);
   return (
     <>
-        <span>{state === false ? 'Contacto no disponible' : 'Contacto En linea'}</span>
-        <button onClick={() => setState(!state)}>{state === false ? 'Conectar' : 'Desconectar'}</button>
+        <span>{status === false ? 'Contacto no disponible' : 'Contacto En linea'}</span>
+        <button onClick={() => setStatus(!status)}>{status === false ? 'Conectar' : 'Desconectar'}</button>
     </>
   )
 }
 
 ComponentB.propTypes = {
-    contact: PropTypes.instanceOf(Contact)
+    status: PropTypes.bool,
 }
 
 export default ComponentB
