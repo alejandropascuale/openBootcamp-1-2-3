1421454ed08796997e4bfef92e7b0a65b91c6573
alejandropascuale <apascuale@outlook.com>
2022-04-20 15:43:34 -0300

    add buton to change online status

--- src/components/ComponentB.jsx
+++ src/components/ComponentB.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Contact } from '../models/contact.class'
 
 function ComponentB({ estado }) {
+    const [state, setState] = useState(estado);
   return (
     <>
-        <span>{ estado ? 'Contacto En Línea' : 'Contacto No Disponible' }</span>
+        <span>{state === false ? 'Contacto no disponible' : 'Contacto En linea'}</span>
+        <button onClick={() => setState(!state)}>{state === false ? 'Conectar' : 'Desconectar'}</button>
     </>
   )
 }
